346516a9325f31c80f7c7db5c491c759a0908a60
Shawn Jones <mryallups@gmail.com>
2013-10-29 21:15:28 -0700

    Fixed config that will allow erros to be displayed and a stack trace
    YAY!

--- application/classes/Kohana/Exception.php
+++ application/classes/Kohana/Exception.php
@@ -15,31 +15,31 @@ class Kohana_Exception extends Kohana_Kohana_Exception {
      * @param   Exception  $e
      * @return  Response
      */
-    public static function response(Exception $e)
-    {
-        Logger::getLogger("error")->error($e->getMessage(), $e);
-
-        if (Kohana::$environment === Kohana::PRODUCTION)
-        {
-            $httpStatus = 500;
-            $code = $e->getCode();
-
-            try {
-                if(is_numeric($code)
-                    && $code > 100
-                    && $code < 600) {
-                    $httpStatus = $code;
-                }
-            } catch (Exception $fe) {
-                Logger::getLogger("default")->error('something really bad happended parsing error', $fe);
-            }
-
-            // call default error page
-            return Response::factory()
-                ->status($httpStatus)
-                ->headers('Location', URL::site('errors/404'));
-        } else {
-            return parent::response($e);
-        }
-    }
+    //public static function response(Exception $e)
+    //{
+    //    Logger::getLogger("error")->error($e->getMessage(), $e);
+    //
+    //    if (Kohana::$environment === Kohana::PRODUCTION)
+    //    {
+    //        $httpStatus = 500;
+    //        $code = $e->getCode();
+    //
+    //        try {
+    //            if(is_numeric($code)
+    //                && $code > 100
+    //                && $code < 600) {
+    //                $httpStatus = $code;
+    //            }
+    //        } catch (Exception $fe) {
+    //            Logger::getLogger("default")->error('something really bad happended parsing error', $fe);
+    //        }
+    //
+    //        // call default error page
+    //        return Response::factory()
+    //            ->status($httpStatus)
+    //            ->headers('Location', URL::site('errors/404'));
+    //    } else {
+    //        return parent::response($e);
+    //    }
+    //}
 }
